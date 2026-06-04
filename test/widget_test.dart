8a2391c82f3002f55f03a796c8dee4a175ce0231
Suraj Chaurasia <suraj84chaurasia@gmail.com>
2026-06-04 12:52:13 +0530

    test: update widget tests

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:rentwheels/main.dart';
+import 'package:gaadisaathi/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
