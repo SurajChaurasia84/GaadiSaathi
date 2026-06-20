c3b7a25d536cb05a4a8ef74f433fbbe7c92cc4ab
Suraj Chaurasia <suraj84chaurasia@gmail.com>
2026-06-20 11:58:49 +0530

    feat(ads): add banner ad widget

---
+++ lib/widgets/ad_banner_widget.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
+
+class AdBannerWidget extends StatefulWidget {
+  const AdBannerWidget({super.key});
+
+  @override
+  State<AdBannerWidget> createState() => _AdBannerWidgetState();
+}
+
+class _AdBannerWidgetState extends State<AdBannerWidget> {
+  BannerAd? _bannerAd;
+  bool _isLoaded = false;
+
+  // The Banner Ad Unit ID provided by the user
+  final String _adUnitId = 'ca-app-pub-9356218156713758/4379704067';
+
+  @override
+  void initState() {
+    super.initState();
+    _loadAd();
+  }
+
+  void _loadAd() {
+    _bannerAd = BannerAd(
+      adUnitId: _adUnitId,
+      request: const AdRequest(),
+      size: AdSize.banner,
+      listener: BannerAdListener(
+        onAdLoaded: (ad) {
+          setState(() {
+            _isLoaded = true;
+          });
+        },
+        onAdFailedToLoad: (ad, err) {
+          debugPrint('BannerAd failed to load: $err');
+          ad.dispose();
+        },
+      ),
+    )..load();
+  }
+
+  @override
+  void dispose() {
+    _bannerAd?.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (_isLoaded && _bannerAd != null) {
+      return Container(
+        alignment: Alignment.center,
+        width: _bannerAd!.size.width.toDouble(),
+        height: _bannerAd!.size.height.toDouble(),
+        color: Theme.of(context).scaffoldBackgroundColor,
+        child: AdWidget(ad: _bannerAd!),
+      );
+    }
+    // Return an empty spacer if not loaded to prevent layout shifts
+    return const SizedBox.shrink();
+  }
+}
