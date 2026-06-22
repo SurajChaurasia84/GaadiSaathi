50df5a92f56b159921428b6ac7175e3a65803ce1
Suraj Chaurasia <suraj84chaurasia@gmail.com>
2026-06-22 17:23:24 +0530

    chore(android): update app build configuration

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -18,7 +18,7 @@ if (keystorePropertiesFile.exists()) {
 android {
     namespace = "com.gaadisaathi.rent.apps"
     compileSdk = flutter.compileSdkVersion
-    // ndkVersion = flutter.ndkVersion
+    ndkVersion = "28.2.13676358"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
